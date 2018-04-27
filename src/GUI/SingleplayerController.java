311ff21a93296cd8510ec9ec3beb70423653e89c
newbrine <35578361+newbrine@users.noreply.github.com>
2018-04-27 16:17:39 -0500

    Add files via upload

--- src/GUI/SingleplayerController.java
+++ src/GUI/SingleplayerController.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
 
 public class SingleplayerController {
 
@@ -25,30 +23,23 @@ public class SingleplayerController {
 	
 	@FXML
 	public void initialize() {
-		File file = new File(System.getProperty("user.dir")+"\\media\\galalite.PNG");
-		Image image = new Image("file:///"+file.getAbsolutePath());
-		gameOne.setTextFill(new ImagePattern(image));
+		
 	}
 	
 	@FXML
 	public void openGameOne() throws IOException {
-		File file = new File(System.getProperty("user.dir")+"\\GalaliteTwo.jar");
-		System.out.println(file.getAbsolutePath());
+		File file = new File("C:\\Users\\Ryan\\Downloads\\Galalite Two\\GalaliteTwo.jar");
 		Desktop.getDesktop().open(file);
 	}
 	
 	@FXML
-	public void openGameTwo() throws IOException {
-		File file = new File(System.getProperty("user.dir")+"\\Matcher.jar");
-		System.out.println(file.getAbsolutePath());
-		Desktop.getDesktop().open(file);
+	public void openGameTwo() {
+		
 	}
 	
 	@FXML
-	public void openGameThree() throws IOException {
-		File file = new File(System.getProperty("user.dir")+"\\Blackhole.jar");
-		System.out.println(file.getAbsolutePath());
-		Desktop.getDesktop().open(file);
+	public void openGameThree() {
+		
 	}
 	
 	@FXML
