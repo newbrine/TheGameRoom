263cab3a707f5e71233614d4f8c368c6d184590b
newbrine <35578361+newbrine@users.noreply.github.com>
2018-04-29 23:15:39 -0500

    Create EndScreenController.java

---
+++ src/gameroom/EndScreenController.java
@@ -0,0 +1,37 @@
+package gameroom;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class EndScreenController{
+	
+	@FXML
+	public Label yourScore;
+	
+	@FXML
+	public Label opponentScore;
+
+	public void initialize() {
+		
+	}
+	
+	public static String getYourScore() {
+		return yourScore.getText();
+	}
+	
+	public static String getOpponentScore() {
+		return opponentScore.getText();
+	}
+	
+	public static void setYourScore(String score) {
+		yourScore.setText(score);
+	}
+	
+	public static void setOpponentScore(String score) {
+		opponentScore.setText(score);
+	}
+}
