b5c6ad4064eda51b6cfed8facb48833292c6a355
Jacob Stack <36282366+DM1818@users.noreply.github.com>
2018-04-30 02:06:46 -0500

    Add files via upload

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -5,16 +5,25 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import gameroom.BadNews;
+
 public class Client {
-	
+
 	private Socket socket;
 
-	public Client(String hostIP, int port, String message) throws IOException {
+	private final String serverIP = "10.253.202.133";
+	private final int portNumber  = 8888;
+	private BadNews badNews = new BadNews();
+
+	public Client(String message) throws IOException {
+
 		try {
-			socket = new Socket(hostIP, port);
+			socket = new Socket(serverIP, portNumber);
 		} catch (UnknownHostException h) {
+			badNews.badNews(h.getMessage());
 			System.out.println(h);
 		} catch (IOException e) {
+			badNews.badNews(e.getMessage());
 			System.out.println(e);
 		}
 		byte[] bytes = message.getBytes();
